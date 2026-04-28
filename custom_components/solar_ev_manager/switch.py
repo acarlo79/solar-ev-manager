2477327ee257bedf9706f9a802592261da48eb7e
acarlo79 <carlo.ambrosio@gmail.com>
2026-04-28 12:44:04 +0100

    Update switch.py

--- custom_components/solar_ev_manager/switch.py
+++ custom_components/solar_ev_manager/switch.py
@@ -3,24 +3,26 @@ import asyncio
 import math
 from homeassistant.components.switch import SwitchEntity
 from homeassistant.helpers.event import async_track_state_change_event
+from homeassistant.helpers.dispatcher import async_dispatcher_send
 from homeassistant.const import STATE_ON, STATE_OFF
 
 _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass, entry, async_add_entities):
-    conf = entry.data
-    async_add_entities([SolarEVManagerSwitch(hass, conf)])
+    async_add_entities([SolarEVManagerSwitch(hass, entry)])
 
 class SolarEVManagerSwitch(SwitchEntity):
-    def __init__(self, hass, conf):
+    def __init__(self, hass, entry):
         self.hass = hass
+        self.entry = entry
+        self.conf = entry.data
         self._attr_name = "Solar EV Manager"
         self._attr_unique_id = "solar_ev_manager_master_switch"
         self._attr_icon = "mdi:solar-power"
         self._is_on = False
-        self.conf = conf
         self._listener = None
         self._current_amps = None
+        self._signal_name = f"solar_ev_update_{entry.entry_id}"
 
     @property
     def is_on(self):
@@ -30,14 +32,10 @@ class SolarEVManagerSwitch(SwitchEntity):
         self._is_on = True
         self.async_write_ha_state()
         
-        # 1. Suspend Octopus
         await self.hass.services.async_call("switch", "turn_off", {"entity_id": self.conf["octopus_switch"]})
         await asyncio.sleep(5)
-        
-        # 2. Wake Tesla & Start Charge
         await self.hass.services.async_call("switch", "turn_on", {"entity_id": self.conf["tesla_switch"]})
         
-        # 3. Start listening to live sensor changes
         self._listener = async_track_state_change_event(
             self.hass, 
             [self.conf["grid_sensor"], self.conf["ev_sensor"], self.conf["voltage_sensor"], self.conf["tesla_cable"]], 
@@ -50,15 +48,14 @@ class SolarEVManagerSwitch(SwitchEntity):
         self._is_on = False
         self.async_write_ha_state()
         
-        # Stop listening to sensors
         if self._listener:
             self._listener() 
             self._listener = None
             
-        # 1. Hand back to Octopus
+        # Broadcast zeros to the UI sensors when turned off
+        async_dispatcher_send(self.hass, self._signal_name, 0.0, 0)
+            
         await self.hass.services.async_call("switch", "turn_on", {"entity_id": self.conf["octopus_switch"]})
-        
-        # 2. Pause Tesla charge
         await self.hass.services.async_call("switch", "turn_off", {"entity_id": self.conf["tesla_switch"]})
 
     async def _calculate_and_adjust(self, event):
@@ -82,20 +79,23 @@ class SolarEVManagerSwitch(SwitchEntity):
             
         excess_watts = (grid * -1) + ev
         target_amps = math.floor(excess_watts / voltage)
-        target_amps = max(0, min(32, target_amps))
+        clamped_amps = max(0, min(32, target_amps))
+        
+        # Broadcast the live math directly to the debug sensors!
+        async_dispatcher_send(self.hass, self._signal_name, excess_watts, clamped_amps)
         
         charge_switch = self.hass.states.get(self.conf["tesla_switch"])
         
-        if target_amps >= 5:
+        if clamped_amps >= 5:
             if charge_switch and charge_switch.state != STATE_ON:
                 await self.hass.services.async_call("switch", "turn_on", {"entity_id": self.conf["tesla_switch"]})
                 
-            if self._current_amps != target_amps:
+            if self._current_amps != clamped_amps:
                 await self.hass.services.async_call("number", "set_value", {
                     "entity_id": self.conf["tesla_amps"],
-                    "value": target_amps
+                    "value": clamped_amps
                 })
-                self._current_amps = target_amps
+                self._current_amps = clamped_amps
         else:
             if charge_switch and charge_switch.state != STATE_OFF:
                 await self.hass.services.async_call("switch", "turn_off", {"entity_id": self.conf["tesla_switch"]})
